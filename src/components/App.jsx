e0d9f7ecee53452562594b7ba1c19b061315b231
roshan6022 <roshansingh6.022@gmail.com>
2024-09-18 08:20:46 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  const listMaking = items.map(todoItem => (
+    <li>{todoItem}</li>
+  ))
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {listMaking}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
